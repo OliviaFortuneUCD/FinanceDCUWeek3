1c4f4ec55a9b9c7895a6041ea0626723fec2a02f
olivia.fortune@griffith.ie <olivia.fortune@griffith.ie>
2021-08-04 15:03:14 +0100

    More Code

--- Python3.py
+++ Python3.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 Cars = pd.read_csv("Cars.csv")
 
-Found = Cars.loc[Cars['Car'].str.contains("Toyota", case=False)]
+Found = Cars.loc[Cars['Car'].str.contains("Honda", case=False)]
 print(Found)
