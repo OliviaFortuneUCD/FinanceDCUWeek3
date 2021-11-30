8a10103f5eeaa444761739a8c876e710ff0e9058
OliviafortuneUCD <Test@test.com>
2021-11-30 01:16:55 +0000

    More Code

--- Python4.py
+++ Python4.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import re
+pd.set_option('display.max_colwidth', -1)
+
 netflix = pd.read_csv("netflix_titles.csv")
 
 
 
-#print(netflix.info())
-#print(netflix.columns)
-#print(netflix.isnull().sum())
-#missing = netflix.isnull().sum().to_frame()
-#print(missing[0:3])
-#print(netflix['release_year'].min() , netflix['release_year'].max())
+
 Found = netflix.loc[netflix['title'].str.contains("Blood", case=False)]
+print('Found')
 print(Found)
+
+Country = netflix['country']
+print(Country)
+#office = netflix[netflix['title'].str.contains('The Office (U.S.)', regex=False)]
+#print(office)
+#print(office['duration'].sum())
+#Actors = netflix.loc[netflix['cast'].str.contains("Steve", case=False ,na=False)]
+#print('Find Steve')
+#print(Actors)
+#CountryFinds = netflix.loc[netflix['country'].str.contains("United States", case=False ,na=False)]
+#print('Country Finds')
+#print(CountryFinds)
