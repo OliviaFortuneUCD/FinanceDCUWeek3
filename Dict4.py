e37865bf94e406f48dd7bc5bb8ec07773057228f
OliviafortuneUCD <Test@test.com>
2021-12-01 18:19:17 +0000

    More Code

---
+++ Dict4.py
@@ -0,0 +1,16 @@
+myCar = {
+  "car1" : {
+    "name" : "Avensis",
+    "year" : 2014
+  },
+  "car2" : {
+    "name" : "Juke",
+    "year" : 2017
+  },
+  "car3" : {
+    "name" : "Star",
+    "year" : 2011
+  }
+}
+
+print(myCar)
