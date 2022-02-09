f518e68fb5d4ec08b9cd23c3d3f8e72bfd9fa63f
OliviafortuneUCD <Test@test.com>
2022-02-09 17:51:18 +0000

    More Code

--- PandasExcel.py
+++ PandasExcel.py
@@ -7,8 +7,9 @@ excel_data_df = pandas.read_excel('NewCars2021.Xlsx', sheet_name='Sheet1')
 #print(excel_data_df)
 
 
-Toyota = excel_data_df.loc[excel_data_df['Car'].str.contains("Toyota", case=False ,na=False)]
-print(Toyota)
+#Toyota = excel_data_df.loc[excel_data_df['Car'].str.contains("Toyota", case=False ,na=False)]
+#print(Toyota)
 
 Tesla= excel_data_df.loc[excel_data_df['Car'].str.contains("Tesla", case=False ,na=False)]
 print(Tesla)
+print(Tesla['Number licensed'].sum())
